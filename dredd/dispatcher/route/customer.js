18c30175b3c125cd351ed102cc356719d1b4249b
ovaphlow <ovaphlow@outlook.com>
2020-05-12 16:34:52 +0800

    close #4

--- dredd/dispatcher/route/customer.js
+++ dredd/dispatcher/route/customer.js
@@ -8,22 +8,81 @@ const router = new Router({
 
 module.exports = router
 
+router.get('/:id', async ctx => {
+  const cnx = await postgres.connect()
+  try {
+    const sql = `
+      select * from dredd.customer where id = $1 limit 1
+    `
+    const result = await cnx.query(sql, [parseInt(ctx.params.id)])
+    ctx.response.body = { message: '', content: result.rowCount === 1 ? result.rows[0] : {} }
+  } catch (err) {
+    console.error(err)
+    ctx.response.body = {message: '服务器错误', content: '' }
+  } finally {
+    cnx.release()
+  }
+})
+
+router.put('/:id', async ctx => {
+  const cnx = await postgres.connect()
+  try {
+    const sql = `
+      update dredd.customer
+      set uuid = $1, name = $2, tel = $3,
+        address_level1 = $4, address_level2 = $5, address_level3 = $6, address_level4 = $7
+      where id = $8
+    `
+    const result = await cnx.query(sql, [
+      ctx.request.body.uuid,
+      ctx.request.body.name,
+      ctx.request.body.tel,
+      ctx.request.body.address_level1,
+      ctx.request.body.address_level2,
+      ctx.request.body.address_level3,
+      ctx.request.body.address_level4,
+
+      parseInt(ctx.params.id)
+    ])
+    ctx.response.body = { message: '', content: result }
+  } catch (err) {
+    console.error(err)
+    ctx.response.body = { message: '服务器错误', content: '' }
+  } finally {
+    cnx.release()
+  }
+})
+
 router.get('/', async ctx => {
-  ctx.response.body = { message: '', content: '111' }
+  const cnx = await postgres.connect()
+  try {
+    const sql = `
+      select * from dredd.customer order by id desc
+    `
+    const result = await cnx.query(sql)
+    ctx.response.body = { message: '', content: result.rows }
+  } catch (err) {
+    console.error(err)
+    ctx.response.body = { message: '服务器错误', content: '' }
+  } finally {
+    cnx.release()
+  }
 })
 
 router.post('/', async ctx => {
-  const client = await postgres.connect()
+  const cnx = await postgres.connect()
   try {
     const sql = `
       insert into
         dredd.customer (uuid, name, tel,
-          address_level1, address_level2, address_level3, address_level4)
+          address_level1, address_level2, address_level3, address_level4,
+          created_at)
         values ($1, $2, $3,
-          $4, $5, $6, $7)
+          $4, $5, $6, $7,
+          current_timestamp)
       returning id
     `
-    const result = await client.query(sql, [
+    const result = await cnx.query(sql, [
       ctx.request.body.uuid,
       ctx.request.body.name,
       ctx.request.body.tel,
@@ -37,6 +96,28 @@ router.post('/', async ctx => {
     console.error(err.stack)
     ctx.response.body = { message: '服务器错误' }
   } finally {
-    client.release()
+    cnx.release()
+  }
+})
+
+router.put('/', async ctx => {
+  const cnx = await postgres.connect()
+  try {
+    const sql = `
+      select *
+      from dredd.customer
+      where position($1 in name) > 0
+        or position($2 in tel) > 0
+    `
+    const result = await cnx.query(sql, [
+      ctx.request.body.filter_name,
+      ctx.request.body.filter_name
+    ])
+    ctx.response.body = { message: '', content: result.rows }
+  } catch (err) {
+    console.error(err)
+    ctx.response.body = { message: '服务器错误', content: '' }
+  } finally {
+    cnx.release()
   }
 })
--- dredd/webapp/src/customer.js
+++ dredd/webapp/src/customer.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route, useParams } from 'react-router-dom'
 import { v5 as uuidv5 } from 'uuid'
+import moment from 'moment'
 
 ReactDOM.render(<Index />, document.getElementById('app'))
 
@@ -10,13 +11,40 @@ function Index() {
     <Router>
       <Switch>
         <Route exact path="/"><List /></Route>
-        <Route path="/新增"><Detail category="新增" /></Route>
+        <Route exact path="/新增"><Detail category="新增" /></Route>
+        <Route path="/:customer_id"><Detail category="编辑" /></Route>
       </Switch>
     </Router>
   )
 }
 
 function List() {
+  const [customer_list, setCustomerList] = useState([])
+  const [filter_name, setFilterName] = useState('')
+
+  const handleFilter = async () => {
+    setCustomerList([])
+    let res = await window.fetch(`/api/customer/`, {
+      method: 'PUT',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ filter_name: filter_name })
+    })
+    res = await res.json()
+    if (res.message) {
+      window.alert(res.message)
+      return
+    }
+    setCustomerList(res.content)
+  }
+
+  useEffect(() => {
+    ;(async () => {
+      let res = await window.fetch(`/api/customer/`)
+      res = await res.json()
+      setCustomerList(res.content)
+    })()
+  }, [])
+
   return (
     <div className="container">
       <h1>CUSTOMER - LIST</h1>
@@ -38,11 +66,77 @@ function List() {
           </li>
         </ul>
       </div>
+
+      <div className="row">
+        <div className="offset-2 col-8">
+          <div className="card shadow">
+            <div className="card-header">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">名称/电话</span>
+                </div>
+                <input type="text" name={filter_name || ''}
+                  className="form-control"
+                  onChange={event => setFilterName(event.target.value)}
+                />
+              </div>
+
+              <div className="mt-2">
+                <div className="btn-group">
+                  <button type="button" className="btn btn-outline-secondary btn-sm"
+                    onClick={() => window.location.reload(true)}
+                  >
+                    <i className="fa fa-fw fa-refresh"></i>
+                    重置
+                  </button>
+                </div>
+
+                <div className="btn-group pull-right">
+                  <button type="button" className="btn btn-outline-info btn-sm"
+                    onClick={handleFilter}
+                  >
+                    <i className="fa fa-fw fa-search"></i>
+                    查询
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="card-body">
+              <div className="list-group">
+                {customer_list.map(it => (
+                  <a key={it.id} href={`#${it.id}`}
+                    className="list-group-item list-group-item-action"
+                  >
+                    <div className="d-flex w-100 justify-content-between">
+                      <h5 className="mb-1">
+                        {it.name}
+                      </h5>
+                      <small>
+                        电话：{it.tel}
+                      </small>
+                    </div>
+                    <ul className="list-inline mb-1">
+                      地址：
+                      <li className="list-inline-item">{it.address_level1}</li>
+                      <li className="list-inline-item">{it.address_level2}</li>
+                      <li className="list-inline-item">{it.address_level3}</li>
+                      <li className="list-inline-item">{it.address_level4}</li>
+                    </ul>
+                    <small>添加于：{moment(it.created_at).format('YYYY-MM-DD')}</small>
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
 function Detail(props) {
+  const { customer_id } = useParams()
   const [name, setName] = useState('')
   const [tel, setTel] = useState('')
   const [address_level1, setAddressLevel1] = useState('')
@@ -63,7 +157,7 @@ function Detail(props) {
       address_level1: address_level1,
       address_level2: address_level2,
       address_level3: address_level3,
-      address_level4: address_level4,
+      address_level4: address_level4
     }
 
     if (props.category === '新增') {
@@ -73,13 +167,45 @@ function Detail(props) {
         body: JSON.stringify(data)
       })
       res = await res.json()
-      console.info(res)
+      if (res.message) {
+        window.alert(res.message)
+        return
+      }
+      window.history.go(-1)
+    } else if (props.category === '编辑') {
+      let res = await window.fetch(`/api/customer/${customer_id}`, {
+        method: 'PUT',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(data)
+      })
+      res = await res.json()
+      if (res.message) {
+        window.alert(res.message)
+        return
+      }
+      window.history.go(-1)
     }
   }
 
+  useEffect(() => {
+    if (props.category === '编辑') {
+      ;(async customer_id => {
+        let res = await window.fetch(`/api/customer/${customer_id}`)
+        res = await res.json()
+        setName(res.content.name)
+        setTel(res.content.tel)
+        setAddressLevel1(res.content.address_level1)
+        setAddressLevel2(res.content.address_level2)
+        setAddressLevel3(res.content.address_level3)
+        setAddressLevel4(res.content.address_level4)
+      })(customer_id)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div className="container">
-      <h1>{props.category} CUSTOMER</h1>
+      <h1>CUSTOMER - {props.category}</h1>
 
       <div className="row justify-content-md-center">
         <ul className="list-inline">
