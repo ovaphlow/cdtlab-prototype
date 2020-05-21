f709b79bf527b74da2afd37318ff33dff4634f8d
ovaphlow <ovaphlow@outlook.com>
2020-05-21 11:01:39 +0800

    eslint home

--- dredd/webapp/src/home/index.jsx
+++ dredd/webapp/src/home/index.jsx
@@ -1,45 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Index />, document.getElementById('app'));
 
 function Index() {
   return (
     <div className="container">
-      <h1><em>DREDD</em></h1>
+      <h1>
+        <em>DREDD</em>
+      </h1>
       <hr />
 
       <div className="row justify-content-md-center">
         <ul className="list-inline">
           <li className="list-inline-item">
             <a href="customer.html">
-              <i className="fa fa-fw fa-address-card"></i>
+              <i className="fa fa-fw fa-address-card" />
               CUSTOMER
             </a>
           </li>
 
           <li className="list-inline-item">
             <a href="staff.html">
-              <i className="fa fa-fw fa-users"></i>
+              <i className="fa fa-fw fa-users" />
               STAFF
             </a>
           </li>
 
           <li className="list-inline-item">
             <a href="user.html#/登录">
-              <i className="fa fa-fw fa-sign-in"></i>
+              <i className="fa fa-fw fa-sign-in" />
               SIGN IN
             </a>
           </li>
 
           <li className="list-inline-item">
             <a href="user.html#/注册">
-              <i className="fa fa-fw fa-user-plus"></i>
+              <i className="fa fa-fw fa-user-plus" />
               SIGN UP
             </a>
           </li>
         </ul>
       </div>
     </div>
-  )
+  );
 }
-
-ReactDOM.render(<Index />, document.getElementById('app'))
